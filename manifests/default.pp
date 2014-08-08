b16e4aa654d4d8030eea96524f951c9320c6d344
Victor Denisov <vdenisov@mirantis.com>
2014-08-07 20:01:22 -0700

    Set up keystone service

--- manifests/default.pp
+++ manifests/default.pp
@@ -14,5 +14,58 @@ vs_bridge { 'br-ex':
 exec { '/usr/bin/apt-get update': }
 
 include ntp
-include database
-include messaging
+#include messaging
+
+$db_root_pass = hiera('mysql_root_pass')
+
+$keystone_db_user = hiera('keystone_db_user')
+$keystone_db_pass = hiera('keystone_db_pass')
+
+$openstack_admin_user = hiera('openstack_admin_user')
+$openstack_admin_pass = hiera('openstack_admin_pass')
+
+class {'::mysql::server':
+	root_password    => $db_root_pass,
+	override_options => {
+		'mysqld' => {
+			'bind-address' => "${::mgmt_ip}",
+			'default-storage-engine' => 'innodb',
+			'collation-server' => 'utf8_general_ci',
+			'init-connect' => "'SET NAMES utf8'",
+			'character-set-server' => "utf8",
+		},
+	},
+	restart         => true,
+	service_enabled => true,
+}
+
+class { 'keystone':
+	verbose             => true,
+	catalog_type        => 'sql',
+	admin_token         => 'ADMIN',
+	database_connection => 'mysql://keystoneUser:keystonePass@localhost/keystone',
+	mysql_module        => '2.3',
+}
+
+# Adds the admin credential to keystone.
+class { 'keystone::roles::admin':
+	email        => 'admin@example.com',
+	password     => $openstack_admin_pass,
+	admin        => $openstack_admin_user,
+	admin_tenant => 'admin',
+}
+
+# Installs the service user endpoint.
+class { 'keystone::endpoint':
+	public_url   => "http://${public_ip}:5000",
+	admin_url    => "http://${mgmt_ip}:35357",
+	internal_url => "http://${mgmt_ip}:5000",
+}
+
+class { 'keystone::db::mysql':
+	user          => $keystone_db_user,
+	password      => $keystone_db_pass,
+	mysql_module  => '2.3',
+	allowed_hosts => '%',
+}
+
--- modules/database/manifests/init.pp
+++ modules/database/manifests/init.pp
@@ -14,33 +14,6 @@ class database {
 		restart         => true,
 		service_enabled => true,
 	}
-	mysql_database { 'test':
-		ensure => 'present',
-	}
-
-	mysql_user { 'testUser@localhost':
-		ensure => 'present'
-	}
-
-	mysql_grant { 'testUser@localhost/test.*':
-		ensure     => 'present',
-		options    => ['GRANT'],
-		privileges => ['ALL'],
-		table      => 'test.*',
-		user       => 'testUser@localhost',
-	}
-
-	mysql_user { 'testUser@%':
-		ensure => 'present'
-	}
-
-	mysql_grant { 'testUser@%/test.*':
-		ensure     => 'present',
-		options    => ['GRANT'],
-		privileges => ['ALL'],
-		table      => 'test.*',
-		user       => 'testUser@%',
-	}
 
 	class { '::mysql::bindings':
 		python_enable => true,
